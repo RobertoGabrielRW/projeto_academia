8253009d443715467a6e3176fa17953f2b423504
Emerson Braga <emersonbraga019@gmail.com>
2025-11-08 18:05:52 -0300

    alteração na classe personal trainer

--- academia/src/test/java/entitys/PersonalTrainner.java
+++ academia/src/test/java/entitys/PersonalTrainner.java
@@ -1,46 +1,34 @@
 package entitys;
+
+import abstracts.Person;
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
-
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import abstracts.Person;
-import org.springframework.data.annotation.Id;
-
 
 @Entity
-@Table(name = "PersonalTrainnerl")
-
+@Table(name = "personal_trainer")
+@PrimaryKeyJoinColumn(name = "person_id")
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonalTrainner extends Person {
 
 
-    @jakarta.persistence.Id
-    private Long id;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idPersonalTrainnerl;
 
-
+    @Column(length = 50)
     private String certification;
 
-    private Integer yearsOfExperience; // anos de experiência
+    private Integer yearsOfExperience;
     private Double hourlyRate;
     private Double rating;
     private String specialties;
     private Boolean available;
-    @OneToOne
-    @JoinColumn(name = "address_id")
-    private Academy address;
     private String scheduleNotes;
 
-    public Academy getAddress() {
-        return address;
-    }
 
-    public void setAddress(Academy address) {
-        this.address = address;
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "academy_id", nullable = false)
+    private Academy academy;
+
+
 }
-
